70e0a8e01a17c7eb7c34a64d641499650ce3fb1e
sateffen <sateffen@gmail.com>
2018-09-08 15:38:27 +0200

    Added test for require

---
+++ test/require.spec.js
@@ -0,0 +1,74 @@
+const NodeModule = require('module');
+const {patchRequire} = require('../src/require');
+
+jest.mock('../src/utils');
+const utils = require('../src/utils');
+
+const originalRequire = NodeModule.prototype.require;
+const confinementSymbol = Symbol('confinement-test-symbol');
+
+describe('require', () => {
+    describe('patchRequire', () => {
+        afterEach(() => {
+            NodeModule.prototype.require = originalRequire;
+        });
+
+        test('It should not throw', () => {
+            const functionToTest = (confinementSymbol) => patchRequire(confinementSymbol);
+
+            expect(functionToTest).not.toThrow();
+        });
+
+        test('It should override the require method with a new one', () => {
+            patchRequire(confinementSymbol);
+
+            expect(NodeModule.prototype.require).not.toBe(originalRequire);
+        });
+    });
+
+    describe('patchRequire patched require version', () => {
+        let requireMock = null;
+
+        beforeEach(() => {
+            module[confinementSymbol] = undefined;
+            requireMock = jest.fn();
+            NodeModule.prototype.require = requireMock;
+            patchRequire(confinementSymbol);
+        });
+
+        afterEach(() => {
+            NodeModule.prototype.require = originalRequire;
+        });
+
+        test('require should call through if no confinement is found', () => {
+            const mockReturn = Math.random().toString(36);
+            requireMock.mockReturnValue(mockReturn);
+            const returnValue = NodeModule.prototype.require.call(module, 'my-test-module');
+
+            expect(returnValue).toBe(mockReturn);
+            expect(utils.isAllowedToCall).not.toHaveBeenCalled();
+        });
+
+        test('require should evaluate the confinement if found by calling isAllowedToCall with it', () => {
+            const confinement = {};
+            module[confinementSymbol] = confinement;
+            const moduleName = 'my-test-module';
+            utils.isAllowedToCall.mockReturnValue(true);
+
+            NodeModule.prototype.require.call(module, moduleName);
+
+            expect(utils.isAllowedToCall).toHaveBeenCalledWith(confinement, moduleName);
+        });
+
+        test('require should throw an error if the confinement forbids loading the module', () => {
+            const confinement = {};
+            module[confinementSymbol] = confinement;
+            const moduleName = 'my-test-module';
+            utils.isAllowedToCall.mockReturnValue(false);
+
+            const callRequire = () => NodeModule.prototype.require.call(module, moduleName);
+
+            expect(callRequire).toThrow();
+        });
+    });
+});

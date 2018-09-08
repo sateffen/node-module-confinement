6d407b9306e446acefbfb02fcafeee2bb8c0d16d
sateffen <sateffen@gmail.com>
2018-09-08 12:05:06 +0200

    Refactored confinedRequire to just get installed when needed

--- src/lib.js
+++ src/lib.js
@@ -1,11 +1,18 @@
 const NodeModule = require('module');
 const ModuleConfinement = require('./moduleconfinement');
 
+let generalConfinementInstalled = false;
+
 /**
  * Installs a proxy for loading all node-modules in confinements
  * @param {Object} aConfinementConfiguration
  */
-function install(aConfinementConfiguration) {
+function installGeneralConfinement(aConfinementConfiguration) {
+    if (generalConfinementInstalled) {
+        throw new Error('General confinement already installed, cannot install twice');
+    }
+
+    generalConfinementInstalled = true;
     NodeModule.prototype.require = new Proxy(NodeModule.prototype.require, {
         apply(aTarget, aThisContext, aArgumentsList) {
             const newModule = Reflect.apply(aTarget, aThisContext, aArgumentsList);
@@ -17,9 +24,46 @@ function install(aConfinementConfiguration) {
     });
 }
 
+/**
+ * Patches the general module prototype with the *confinedRequire* method
+ */
+function patchConfinedRequire() {
+    NodeModule.prototype.confinedRequire = function loadConfinedModule(aPath, aConfinementConfiguration) {
+        // first determine the file to lad
+        const fileToLoad = NodeModule._resolveFilename(aPath, this, false);
+
+        // and than instanciate the file
+        const newModuleInstance = new NodeModule(aPath, this);
+        newModuleInstance.confinementDefinition = new ModuleConfinement(aConfinementConfiguration);
+
+        // next we need to publish this module, so the outer world knows about this
+        NodeModule._cache[fileToLoad] = newModuleInstance;
+
+        // then we try to load the module
+        let threw = true;
+        try {
+            newModuleInstance.load(fileToLoad);
+            threw = false;
+        }
+        // and if loading the module failed, we delete it from cache.
+        // Important: We don't need a catch(e) block, because we don't want to mutate
+        // the error-Object! If we would catch and rethrow it, the stacktrace would
+        // contain this code, and that's useless
+        finally {
+            if (threw) {
+                delete NodeModule._cache[fileToLoad];
+            }
+        }
+
+        // finally we return the exports of the generated module, because that's why this
+        // function was called
+        return newModuleInstance.exports;
+    };
+}
+
 module.exports = {
-    install,
+    installGeneralConfinement,
+    patchConfinedRequire,
 };
 
 require('./require');
-require('./confinedrequire');
